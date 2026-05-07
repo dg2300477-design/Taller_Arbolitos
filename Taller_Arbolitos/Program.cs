32a562eaf0ccecfac51a43f84322cb44a6e737d1
GIO_77 <dg2300477@gmail.com>
2026-05-07 11:40:51 -0500

    ZD

--- Taller_Arbolitos/Program.cs
+++ Taller_Arbolitos/Program.cs
@@ -9,6 +9,7 @@ namespace Taller_Arbolitos
         static void Main()
         {
             //HOLA XDXD
+            //XDXDXDXDXD
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
